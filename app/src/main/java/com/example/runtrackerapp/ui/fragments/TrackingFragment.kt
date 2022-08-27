2e22d822e0cd2bb947c4204acadd02459d8bc942
yodgor777 <kyodgorbek@gmail.com>
2022-08-27 19:48:33 +0200

    Setup GoogleMaps

--- app/src/main/java/com/example/runtrackerapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runtrackerapp/ui/fragments/TrackingFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runtrackerapp.databinding.FragmentTrackingBinding
 import com.example.runtrackerapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +17,7 @@ class TrackingFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels()
     private var _binding: FragmentTrackingBinding? = null
     private val binding get() = _binding!!
+    private var map: GoogleMap? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,6 +32,41 @@ class TrackingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
 
+        binding.mapView.getMapAsync {
+            map = it
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView?.onSaveInstanceState(outState)
+    }
+
 }
